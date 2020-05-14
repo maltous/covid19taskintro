dca71afdc4566ee5c7bc5ec31bc272cfeeb7a7bb
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-05-14 11:19:19 +0300

    covid19taskintro

---
+++ story_content/user.js
@@ -0,0 +1,47 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5oTinVpeHYd":
+        Script1();
+        break;
+      case "6pxtfawhlkt":
+        Script2();
+        break;
+      case "5b964VPtB2g":
+        Script3();
+        break;
+      case "5bckAfQRTse":
+        Script4();
+        break;
+      case "5pHHk0pDYqp":
+        Script5();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script4()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script5()
+{
+  window.print();
+}
+
